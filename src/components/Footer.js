d93ef8048124528700f6d5d83eca666086966324
Suraj Singh <103835667+suraj-s13@users.noreply.github.com>
2022-09-02 23:19:19 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,10 +13,10 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Soumyajit Behera</h3>
+          <h3>Designed and Developed by Suraj Singh</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} SB</h3>
+          <h3>Copyright © {year} SS</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
